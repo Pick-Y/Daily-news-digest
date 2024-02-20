eb98877164dc291a8bac094b31004a44ef8166ed
Pick-Y <a.scarcella.as@gmail.com>
2024-02-21 02:46:21 +1100

    fixed bug - gmail account required specific password to be generated in google account

--- sendmail.py
+++ sendmail.py
@@ -1,15 +1,25 @@
 import smtplib,ssl
+from email.message import EmailMessage
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "YOUR EMAIL"
-    password = "YOUR PASSWORD"
+    password = "YOUR PASSWORD" #This email must be generated in your google account
 
-    receiver = "YOUR EMAIL"
+    receiver = "YOUR PASSWORD"
+
+    subject = "New Article"
+    body = message
+
+    em = EmailMessage()
+    em['From'] = username
+    em['To'] = receiver
+    em['Subject'] = subject
+    em.set_content(body)
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, receiver, message)
+        server.sendmail(username, receiver, em.as_string())
